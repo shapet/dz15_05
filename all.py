702d47eeb1c7fce079b07cd2a163fe3263a8b140
Your Name <shapettt@gmail/com>
2020-05-14 23:18:47 +0300

    first commit

---
+++ all.py
@@ -0,0 +1,22 @@
+def generator(b):    
+    for i in range(b+1):
+        yield i
+        if i % 3 == 0:
+            yield('vas')
+            
+            
+            
+        
+r = generator(100)
+print(next(r))
+print(next(r))
+print(next(r))
+print(next(r))
+print(next(r))
+print(next(r))
+print(next(r))
+print(next(r))
+print(next(r))
+print(next(r))
+print(next(r))
+print(next(r))
---
+++ dz/lesson4/data_input.py
@@ -0,0 +1,9 @@
+import os
+
+pass
+
+user_weigh = input("введите ваш вес: ")
+#user_heigh = input("введите ваш рост: ")
+
+
+print("summa :", user_input)
